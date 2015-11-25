8c34ab4d457a5225c20e629046afafb9849e5730
Constantine Yurevich <yurevichcv@gmail.com>
2015-11-25 18:00:46 +0300

    karma test

---
+++ karma.conf.js
@@ -0,0 +1,146 @@
+module.exports = function(config) {
+  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+    process.exit(1)
+  }
+
+  var customLaunchers = {
+    slChromeWinXp: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows XP'
+    },
+    slIe10Win7: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '10'
+    },
+    slIe9Win7: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '9'
+    },
+    slIe8Win7: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '8'
+    },
+    slIe11Win10: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 10',
+      version: '11'
+    },
+    slME25Win10: {
+      base: 'SauceLabs',
+      browserName: 'microsoft edge',
+      platform: 'Windows 10',
+      version: '25'
+    },
+    slFfLinux: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'Linux'
+    },
+    slSafariOsx: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.8'
+    },
+    slSafariOsx11: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.11'
+    }
+  };
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'tap'],
+
+    sauceLabs: {
+      testName: 'Digital Data Manager Unit Tests',
+      recordScreenshots: false,
+      connectOptions: {
+        port: 5757,
+        logfile: 'sauce_connect.log'
+      },
+      public: 'public'
+    },
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*.js',
+      'src/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/**/*.js': ['babel', 'browserify'],
+      'test/**/*.js': ['babel', 'browserify']
+    },
+
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015'],
+        sourceMap: 'inline'
+      }
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'saucelabs'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // Increase timeout in case connection in CI is slow
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers),
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultanous
+    concurrency: Infinity
+  });
+};
