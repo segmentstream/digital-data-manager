0d19b07b200fe5913e6e04859b223c5e4fed4297
Constantine Yurevich <yurevichcv@gmail.com>
2016-01-27 17:48:47 +0300

    mocha update

--- test/index.test.js
+++ test/index.test.js
@@ -2,15 +2,15 @@ import './../src/polyfill.js';
 
 //tests
 import './ddManagerSpec.js';
-//import './AutoEventsSpec.js';
-//import './DDHelperSpec.js';
-//import './EventManagerSpec.js';
-//import './EventDataEnricherSpec.js';
-//import './DigitalDataEnricherSpec.js';
+import './AutoEventsSpec.js';
+import './DDHelperSpec.js';
+import './EventManagerSpec.js';
+import './EventDataEnricherSpec.js';
+import './DigitalDataEnricherSpec.js';
 
 //integrations
-//import './integrations/GoogleAnalyticsSpec.js';
-//import './integrations/GoogleTagManagerSpec.js';
-//import './integrations/DrivebackSpec.js';
-//import './integrations/RetailRocketSpec.js';
-//import './integrations/FacebookPixelSpec.js';
+import './integrations/GoogleAnalyticsSpec.js';
+import './integrations/GoogleTagManagerSpec.js';
+import './integrations/DrivebackSpec.js';
+import './integrations/RetailRocketSpec.js';
+import './integrations/FacebookPixelSpec.js';
