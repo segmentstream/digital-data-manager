5a1b4a56b2d38b26f8864bc0f7985b609d07c0b6
Constantine Yurevich <yurevichcv@gmail.com>
2016-07-28 12:04:27 +0300

    set node version

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ import './../src/polyfill.js';
 
 //tests
 import './ddManagerSpec.js';
-import './AutoEventsSpec.js';
+//import './AutoEventsSpec.js';
 //import './DDHelperSpec.js';
 //import './EventManagerSpec.js';
 //import './EventDataEnricherSpec.js';
