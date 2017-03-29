c7c96615986cdfaf4fcd36f0cac75440b636f4b7
Constantine Yurevich <yurevichcv@gmail.com>
2017-03-29 18:49:54 +0300

    version bump

--- src/ddManager.js
+++ src/ddManager.js
@@ -242,7 +242,7 @@ function _initializeIntegrations(settings) {
 
 ddManager = {
 
-  VERSION: '1.2.23',
+  VERSION: '1.2.24',
 
   setAvailableIntegrations: (availableIntegrations) => {
     _availableIntegrations = availableIntegrations;
