3b86d0be94e627a008139f4da28b57aeedd28e26
Constantine Yurevich <yurevichcv@gmail.com>
2017-04-17 23:09:28 +0300

    test

--- src/functions/loadScript.js
+++ src/functions/loadScript.js
@@ -35,7 +35,7 @@ export default function(options, fn) {
   }
 
   const addScriptToHead = () => {
-    console.warn('addScriptToHead');
+    console.warn('addScriptToHead', script);
     const firstScript = document.getElementsByTagName('script')[0];
     firstScript.parentNode.insertBefore(script, firstScript);
   };
