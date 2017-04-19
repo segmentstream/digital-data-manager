700fdcb004964c8f807faed516cb2bbaf4dd063c
Constantine Yurevich <yurevichcv@gmail.com>
2017-04-19 12:33:14 +0300

    weborama integration finalized

--- src/integrations/Weborama.js
+++ src/integrations/Weborama.js
@@ -5,7 +5,7 @@ class Weborama extends Integration {
   constructor(digitalData, options) {
     const optionsWithDefaults = Object.assign({
       siteId: '',
-      eventTags: {},
+      eventPixels: {},
       /* example:
       eventPixels: {
         'Viewed Product Detail': '470'
@@ -15,22 +15,13 @@ class Weborama extends Integration {
 
     super(digitalData, optionsWithDefaults);
 
+    this.SEMANTIC_EVENTS = Object.keys(this.getOption('eventPixels'));
     this._isLoaded = false;
-
-    this.tagEvents = Object.keys(this.getOption('eventTags'));
-    this.SEMANTIC_EVENTS = [];
-
-    for (const tagEvent of this.tagEvents) {
-      const tagOptions = this.getOption('eventTags')[tagEvent];
-      if (tagOptions) {
-        this.SEMANTIC_EVENTS.push(tagEvent);
-      }
-    }
 
     this.addTag({
       type: 'img',
       attr: {
-        src: `https://rtbprojects.solution.weborama.fr/fcgi-bin/dispatch.fcgi?a.A=co&a.si={{ siteId }}&a.cp={{ conversionPage }}&a.ct=d`,
+        src: `https://rtbprojects.solution.weborama.fr/fcgi-bin/dispatch.fcgi?a.A=co&a.si=${options.siteId}&a.cp={{ conversionId }}&a.ct=d`,
       },
     });
   }
@@ -49,16 +40,10 @@ class Weborama extends Integration {
   }
 
   trackEvent(event) {
-    const eventTags = this.getOption('eventTags');
-    const tagOptions = eventTags[event.name];
-
-    if (tagOptions) {
-      const siteId = this.getOption('siteId');
-      const conversionPage = tagOptions.conversionPage;
-
-      const tagParams = { siteId, conversionPage };
-
-      this.load(tagParams);
+    const eventPixels = this.getOption('eventPixels');
+    const conversionId = eventPixels[event.name];
+    if (conversionId) {
+      this.load({ conversionId });
     }
   }
 }
