33575c5a1816964c4746f4dfb870072f5a94edda
Constantine Yurevich <yurevichcv@gmail.com>
2017-04-21 17:05:01 +0300

    test

--- test/index.test.js
+++ test/index.test.js
@@ -20,7 +20,7 @@ import './integrations/GoogleAnalyticsSpec.js';
 import './integrations/GoogleTagManagerSpec.js';
 import './integrations/GoogleAdWordsSpec.js';
 import './integrations/DrivebackSpec.js';
-// import './integrations/RetailRocketSpec.js';
+import './integrations/RetailRocketSpec.js';
 // import './integrations/FacebookPixelSpec.js';
 // import './integrations/SegmentStreamSpec.js';
 // import './integrations/SendPulseSpec.js';
