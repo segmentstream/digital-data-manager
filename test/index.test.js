e8f95e2c326f0d9e2098cfdc5058ecf8bfb5d942
Constantine Yurevich <yurevichcv@gmail.com>
2017-04-21 17:20:10 +0300

    test

--- test/index.test.js
+++ test/index.test.js
@@ -21,12 +21,12 @@ import './integrations/GoogleTagManagerSpec.js';
 import './integrations/GoogleAdWordsSpec.js';
 import './integrations/DrivebackSpec.js';
 import './integrations/RetailRocketSpec.js';
-import './integrations/FacebookPixelSpec.js';
+// import './integrations/FacebookPixelSpec.js';
 // import './integrations/SegmentStreamSpec.js';
 // import './integrations/SendPulseSpec.js';
-// import './integrations/OWOXBIStreamingSpec.js'
-// import './integrations/CriteoSpec.js';
-// import './integrations/MyTargetSpec.js';
+import './integrations/OWOXBIStreamingSpec.js'
+import './integrations/CriteoSpec.js';
+import './integrations/MyTargetSpec.js';
 import './integrations/YandexMetricaSpec.js';
 import './integrations/VkontakteSpec.js';
 import './integrations/EmarsysSpec.js';
