a533f374d7516bd37b5ca82fabd5af8095cb06d4
Constantine Yurevich <yurevichcv@gmail.com>
2017-05-02 19:02:07 +0300

    enhanced links tracking

--- src/trackers/trackLink.js
+++ src/trackers/trackLink.js
@@ -3,12 +3,6 @@ import isMeta from './../functions/isMeta';
 import preventDefault from './../functions/preventDefault';
 import domQuery from './../functions/domQuery';
 
-const namedTrackers = {};
-
-function isElement(el) {
-  return (el && el.nodeType === 1);
-}
-
 function applyHandler(event, el, handler) {
   const href = el.getAttribute('href')
     || el.getAttributeNS('http://www.w3.org/1999/xlink', 'href')
@@ -34,59 +28,30 @@ function onClick(el, handler) {
   };
 }
 
-class LinkTracker {
-  constructor() {
-    this.trackers = [];
-  }
-
-  addTracker(el, handler) {
-    if (!isElement(el)) {
-      throw new TypeError('Must pass HTMLElement to `ddManager.trackLink`.');
-    }
-    const onClickHandler = onClick(el, handler);
-    bind(el, 'click', onClickHandler);
-
-    this.trackers.push([el, onClickHandler]);
-  }
-
-  reset() {
-    for (const tracker of this.trackers) {
-      const [el, handler] = tracker;
-      unbind(el, 'click', handler);
-    }
-  }
-}
-
-export default function trackLink(links, handler, trackingId) {
-  if (!links) return;
-  if (typeof links === 'string') {
-    links = domQuery(links);
-  } else if (isElement(links)) {
-    links = [links];
-  } else if (links.toArray) { // handles jquery
-    links = links.toArray();
-  }
+export default function trackLink(selector, handler) {
+  if (!selector) return;
 
   if (typeof handler !== 'function') {
     throw new TypeError('Must pass function handler to `ddManager.trackLink`.');
   }
 
-  let linkTracker;
-  if (trackingId) {
-    linkTracker = namedTrackers[trackingId];
-    if (linkTracker) {
-      linkTracker.reset();
-    }
-  }
-  
-  if (!linkTracker) {
-    linkTracker = new LinkTracker();
-    namedTrackers[trackingId] = linkTracker;
-  }
+  const trackedLinks = [];
 
-  for (const el of links) {
-    linkTracker.addTracker(el, handler);
-  }
+  bind(window.document, 'click', function bindClickListeners() {
+    const links = domQuery(selector);
+    for (const el of links) {
+      const onClickHandler = onClick(el, handler);
+      bind(el, 'click', onClickHandler);
+      trackedLinks.push([el, onClickHandler]);
+    }
+  }, true); // capturing phase
+
+  bind(window.document, 'click', function unbindClickListeners() {
+    for (const trackedLink of trackedLinks) {
+      const [el, onClickHandler] = trackedLink;
+      unbind(el, 'click', onClickHandler);
+    }
+  }); // bubbling phase
 
   return;
 }
