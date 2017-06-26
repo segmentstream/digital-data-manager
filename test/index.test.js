a764c29c6d65f5fdf17f933200badaab4d315621
Constantine Yurevich <yurevichcv@gmail.com>
2017-06-26 14:32:17 +0300

    test

--- test/index.test.js
+++ test/index.test.js
@@ -15,30 +15,30 @@ import './DigitalDataEnricherSpec.js';
 import './EventValidatorSpec.js';
 
 // enrichments & events
-import './enrichments/CustomEnricherSpec';
+// import './enrichments/CustomEnricherSpec';
 // import './events/CustomEventsSpec';
 
 // trackers
-import './trackers/trackLinkSpec.js';
+// import './trackers/trackLinkSpec.js';
 
 // integrations
-import './integrations/GoogleAnalyticsSpec.js';
-import './integrations/GoogleTagManagerSpec.js';
-import './integrations/GoogleAdWordsSpec.js';
-import './integrations/DrivebackSpec.js';
-import './integrations/RetailRocketSpec.js';
-import './integrations/FacebookPixelSpec.js';
-import './integrations/SegmentStreamSpec.js';
-import './integrations/SendPulseSpec.js';
-import './integrations/OWOXBIStreamingSpec.js'
-import './integrations/CriteoSpec.js';
-import './integrations/MyTargetSpec.js';
-import './integrations/YandexMetricaSpec.js';
-import './integrations/VkontakteSpec.js';
-import './integrations/EmarsysSpec.js';
-import './integrations/OneSignalSpec.js';
-import './integrations/SociomanticSpec.js';
-import './integrations/MindboxSpec.js';
-import './integrations/DoubleClickFloodlightSpec.js';
-import './integrations/RTBHouseSpec.js';
-import './integrations/SolowaySpec.js';
+// import './integrations/GoogleAnalyticsSpec.js';
+// import './integrations/GoogleTagManagerSpec.js';
+// import './integrations/GoogleAdWordsSpec.js';
+// import './integrations/DrivebackSpec.js';
+// import './integrations/RetailRocketSpec.js';
+// import './integrations/FacebookPixelSpec.js';
+// import './integrations/SegmentStreamSpec.js';
+// import './integrations/SendPulseSpec.js';
+// import './integrations/OWOXBIStreamingSpec.js'
+// import './integrations/CriteoSpec.js';
+// import './integrations/MyTargetSpec.js';
+// import './integrations/YandexMetricaSpec.js';
+// import './integrations/VkontakteSpec.js';
+// import './integrations/EmarsysSpec.js';
+// import './integrations/OneSignalSpec.js';
+// import './integrations/SociomanticSpec.js';
+// import './integrations/MindboxSpec.js';
+// import './integrations/DoubleClickFloodlightSpec.js';
+// import './integrations/RTBHouseSpec.js';
+// import './integrations/SolowaySpec.js';
