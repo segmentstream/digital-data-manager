f6132fb821762215a88518e8553a22ac744fff80
Constantine Yurevich <yurevichcv@gmail.com>
2017-06-27 14:51:23 +0300

    test

--- test/index.test.js
+++ test/index.test.js
@@ -41,4 +41,4 @@ import './integrations/SociomanticSpec.js';
 import './integrations/MindboxSpec.js';
 import './integrations/DoubleClickFloodlightSpec.js';
 import './integrations/RTBHouseSpec.js';
-// import './integrations/SolowaySpec.js';
+import './integrations/SolowaySpec.js';
