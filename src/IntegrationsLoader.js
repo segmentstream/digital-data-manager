a8cf1846e6f235f33b49481df4c75e655e0e4b5f
Constantine Yurevich <yurevichcv@gmail.com>
2017-07-31 11:43:43 +0300

    priorities first commit

---
+++ src/IntegrationsLoader.js
@@ -0,0 +1,151 @@
+import size from './functions/size';
+import each from './functions/each';
+import after from './functions/after';
+import { warn } from './functions/safeConsole';
+import Integration from './Integration';
+import clone from './functions/clone';
+import { bind } from './functions/eventListener';
+
+/**
+ * @type {Object}
+ * @private
+ */
+let _availableIntegrations;
+
+/**
+ * @type {Object}
+ * @private
+ */
+let _integrations = {};
+
+const IntegrationsLoader = {
+  setAvailableIntegrations: (availableIntegrations) => {
+    _availableIntegrations = availableIntegrations;
+  },
+
+  addIntegration: (name, integration, ddManager) => {
+    if (!integration instanceof Integration || !name) {
+      throw new TypeError('attempted to add an invalid integration');
+    }
+
+    integration.setName(name);
+    integration.setDDManager(ddManager);
+
+    _integrations[name] = integration;
+  },
+
+  getIntegration: (name) => {
+    return _integrations[name];
+  },
+
+  getIntegrations: () => {
+    return _integrations;
+  },
+
+  addIntegrations: (integrationSettings, ddManager) => {
+    if (integrationSettings) {
+      if (Array.isArray(integrationSettings)) {
+        for (const integrationSetting of integrationSettings) {
+          const name = integrationSetting.name;
+          const options = clone(integrationSetting.options, true);
+          if (typeof _availableIntegrations[name] === 'function') {
+            const integration = new _availableIntegrations[name](ddManager.getDigitalData(), options || {});
+            IntegrationsLoader.addIntegration(name, integration, ddManager);
+          }
+        }
+      } else {
+        each(integrationSettings, (name, options) => {
+          if (typeof _availableIntegrations[name] === 'function') {
+            const integration = new _availableIntegrations[name](ddManager.getDigitalData(), clone(options, true));
+            IntegrationsLoader.addIntegration(name, integration, ddManager);
+          }
+        });
+      }
+    }
+  },
+
+  initializeIntegrations(settings) {
+    const version = settings.version;
+
+    // initialize integrations
+    if (size(_integrations) > 0) {
+      each(_integrations, (name, integration) => {
+        if (
+          !integration.isLoaded()
+          || integration.getOption('noConflict')
+          || integration.allowNoConflictInitialization()
+        ) {
+          integration.initialize(version);
+          integration.setInitialized(true);
+        } else {
+          warn(`Integration "${name}" can't be initialized properly because of the conflict`);
+        }
+      });
+    }
+  },
+
+  loadIntegrations: (integrationsPriority, pageLoadTimeout, callback) => {
+    integrationsPriority = integrationsPriority || {};
+    const beforeList = integrationsPriority.before || [];
+    const afterList = integrationsPriority.after || [];
+
+    const onLoad = () => {
+      callback();
+    };
+    const loaded = after(size(_integrations), onLoad);
+
+    // before
+    IntegrationsLoader.loadIntegrationsFromList(beforeList, loaded);
+
+    // after
+    const pageLoaded = window.document.readyState === 'complete';
+    let integrationsLoaded = false;
+
+    if (!pageLoaded) {
+      // set page load timeout
+      const timeoutId = setTimeout(() => {
+        if (integrationsLoaded) return;
+        integrationsLoaded = true;
+        IntegrationsLoader.loadIntegrationsFromList(afterList, loaded);
+      }, pageLoadTimeout || 3000);
+
+      // wait for page load
+      bind(window, 'load', () => {
+        if (integrationsLoaded) return;
+        integrationsLoaded = true;
+        clearTimeout(timeoutId);
+        IntegrationsLoader.loadIntegrationsFromList(afterList, loaded);
+      });
+    } else {
+      IntegrationsLoader.loadIntegrationsFromList(afterList, loaded);
+    }
+  },
+
+  loadIntegrationsFromList(list, loaded) {
+    for (const integrationName of list) {
+      IntegrationsLoader.loadIntegration(integrationName, loaded);
+    }
+  },
+
+  loadIntegration(integrationName, callback) {
+    if (_integrations[integrationName]) {
+      const integration = _integrations[integrationName];
+      if (!integration.isLoaded()) {
+        integration.load(integration.onLoad);
+        integration.once('load', callback);
+      } else {
+        callback();
+      }
+    }
+  },
+
+  reset: () => {
+    each(_integrations, (name, integration) => {
+      integration.removeAllListeners();
+      integration.reset();
+    });
+    _integrations = {};
+  },
+};
+
+export default IntegrationsLoader;
