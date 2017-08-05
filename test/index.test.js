705c0f914d2157fec1888eaf1a996686f7e7536a
Constantine Yurevich <yurevichcv@gmail.com>
2017-08-05 15:16:49 +0300

    test

--- test/index.test.js
+++ test/index.test.js
@@ -1,45 +1,45 @@
 window.localStorage.clear();
-// console.error = () => {};
+console.error = () => {};
 
-window.__DEV_MODE__ = true; // disable catching exceptions
+// window.__DEV_MODE__ = true; // disable catching exceptions
 
 import './../src/polyfill.js';
 
 // tests
-// import './ddManagerSpec.js';
-// import './DDHelperSpec.js';
-// import './DDStorageSpec.js';
-// import './EventManagerSpec.js';
-// import './EventDataEnricherSpec.js';
-// import './DigitalDataEnricherSpec.js';
-// import './EventValidatorSpec.js';
+import './ddManagerSpec.js';
+import './DDHelperSpec.js';
+import './DDStorageSpec.js';
+import './EventManagerSpec.js';
+import './EventDataEnricherSpec.js';
+import './DigitalDataEnricherSpec.js';
+import './EventValidatorSpec.js';
 
-// // enrichments & events
-// import './enrichments/CustomEnricherSpec';
-// import './events/CustomEventsSpec';
-// import './scripts/CustomScriptsSpec';
+// enrichments & events
+import './enrichments/CustomEnricherSpec';
+import './events/CustomEventsSpec';
+import './scripts/CustomScriptsSpec';
 
-// // trackers
-// import './trackers/trackLinkSpec.js';
+// trackers
+import './trackers/trackLinkSpec.js';
 
-// // integrations
+// integrations
 import './integrations/GoogleAnalyticsSpec.js';
-// import './integrations/GoogleTagManagerSpec.js';
-// import './integrations/GoogleAdWordsSpec.js';
-// import './integrations/DrivebackSpec.js';
-// import './integrations/RetailRocketSpec.js';
-// import './integrations/FacebookPixelSpec.js';
-// import './integrations/SegmentStreamSpec.js';
-// import './integrations/SendPulseSpec.js';
-// import './integrations/OWOXBIStreamingSpec.js'
-// import './integrations/CriteoSpec.js';
-// import './integrations/MyTargetSpec.js';
-// import './integrations/YandexMetricaSpec.js';
-// import './integrations/VkontakteSpec.js';
-// import './integrations/EmarsysSpec.js';
-// import './integrations/OneSignalSpec.js';
-// import './integrations/SociomanticSpec.js';
-// import './integrations/MindboxSpec.js';
-// import './integrations/DoubleClickFloodlightSpec.js';
-// import './integrations/RTBHouseSpec.js';
-// import './integrations/SolowaySpec.js';
+import './integrations/GoogleTagManagerSpec.js';
+import './integrations/GoogleAdWordsSpec.js';
+import './integrations/DrivebackSpec.js';
+import './integrations/RetailRocketSpec.js';
+import './integrations/FacebookPixelSpec.js';
+import './integrations/SegmentStreamSpec.js';
+import './integrations/SendPulseSpec.js';
+import './integrations/OWOXBIStreamingSpec.js'
+import './integrations/CriteoSpec.js';
+import './integrations/MyTargetSpec.js';
+import './integrations/YandexMetricaSpec.js';
+import './integrations/VkontakteSpec.js';
+import './integrations/EmarsysSpec.js';
+import './integrations/OneSignalSpec.js';
+import './integrations/SociomanticSpec.js';
+import './integrations/MindboxSpec.js';
+import './integrations/DoubleClickFloodlightSpec.js';
+import './integrations/RTBHouseSpec.js';
+import './integrations/SolowaySpec.js';
