fcd42695adb2f379774dbfcf390fa6d37486e430
Constantine Yurevich <yurevichcv@gmail.com>
2018-01-26 18:01:43 +0300

    implementation

--- src/integrations/PushWorld.js
+++ src/integrations/PushWorld.js
@@ -1,11 +1,11 @@
+import cookie from 'js-cookie';
 import Integration from './../Integration';
 
 class PushWorld extends Integration {
   constructor(digitalData, options) {
     const optionsWithDefaults = Object.assign({
-      namespace: undefined,
-      sessionStreaming: false,
-      sessionIdDimension: undefined,
+      domain: undefined,
+      platformCode: undefined,
     }, options);
 
     super(digitalData, optionsWithDefaults);
@@ -19,12 +19,34 @@ class PushWorld extends Integration {
   }
 
   initialize() {
+    window.pw = {
+      websiteId: this.getOption('platformCode'),
+      date: Date.now(),
+    };
     this.enrichDigitalData();
+    document.addEventListener('pw:subscribe:allow', (event) => {
+      this.digitalData.changes.push(['user.pushNotifications.isSubscribed', true]);
+      this.digitalData.changes.push(['user.pushNotifications.userId', event.detail.device_id]);
+    }, false);
+    document.addEventListener('pw:subscribe:deny', () => {
+      this.digitalData.changes.push(['user.pushNotifications.isDenied', true]);
+    }, false);
   }
 
   enrichDigitalData() {
-    const statusCookie = 'pw_status_'
-    const isSubscribed;
+    const status = cookie.get(`pw_status_${this.getOption('platformCode')}`);
+    let isSubscribed = false;
+    switch (status) {
+      case 'allow':
+        isSubscribed = true;
+        this.digitalData.changes.push(['user.pushNotifications.userId', cookie.get('pw_deviceid')]);
+        break;
+      case 'deny':
+        this.digitalData.changes.push(['user.pushNotifications.isDenied', true]);
+        break;
+      default:
+        break;
+    }
     this.digitalData.changes.push(['user.pushNotifications.isSubscribed', isSubscribed]);
   }
 }
