0fd09ed25b6581ad88ffd8990e7b42500ffdeca3
Constantine Yurevich <yurevichcv@gmail.com>
2018-03-27 16:57:32 +0300

    OneSignal push events

--- src/ddManager.js
+++ src/ddManager.js
@@ -224,7 +224,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.86',
+  VERSION: '1.2.87',
 
   setAvailableIntegrations: (availableIntegrations) => {
     IntegrationsLoader.setAvailableIntegrations(availableIntegrations);
