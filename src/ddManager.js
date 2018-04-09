065edff396962f3c07898f282537a03a951b5f62
Constantine Yurevich <yurevichcv@gmail.com>
2018-04-09 18:11:36 +0300

    version 1.2.91 bump

--- src/ddManager.js
+++ src/ddManager.js
@@ -224,7 +224,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.90',
+  VERSION: '1.2.91',
 
   setAvailableIntegrations: (availableIntegrations) => {
     IntegrationsLoader.setAvailableIntegrations(availableIntegrations);
