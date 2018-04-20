e59b90c4adeadc794cecfe31e8d9cf9e21d5dcbf
Constantine Yurevich <yurevichcv@gmail.com>
2018-04-20 12:59:58 +0300

    Soloway completed transaction improvement - version 1.2.99 bump

--- src/ddManager.js
+++ src/ddManager.js
@@ -210,7 +210,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.98',
+  VERSION: '1.2.99',
 
   setAvailableIntegrations: (availableIntegrations) => {
     IntegrationsLoader.setAvailableIntegrations(availableIntegrations);
