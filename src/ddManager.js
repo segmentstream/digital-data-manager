c33f4fdbf8650d4ecdd1448dede4da3a58fd52e9
Constantine Yurevich <yurevichcv@gmail.com>
2018-04-25 17:37:08 +0300

    OWOX url update version 1.2.100 bump

--- src/ddManager.js
+++ src/ddManager.js
@@ -210,7 +210,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.99',
+  VERSION: '1.2.100',
 
   setAvailableIntegrations: (availableIntegrations) => {
     IntegrationsLoader.setAvailableIntegrations(availableIntegrations);
