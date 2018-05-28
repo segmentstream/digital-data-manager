4c55a5972402b5e172d81fcb83eab1a63547615c
Constantine Yurevich <yurevichcv@gmail.com>
2018-05-28 15:59:28 +0300

    - requirejs conflict fixed - version 1.2.113 bump

--- src/ddManager.js
+++ src/ddManager.js
@@ -210,7 +210,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.112',
+  VERSION: '1.2.113',
 
   setAvailableIntegrations: (availableIntegrations) => {
     IntegrationsLoader.setAvailableIntegrations(availableIntegrations);
