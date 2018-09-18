9927306c2d24f1efa290175bac8c0bac940dd491
Kirill Temnov <allselead@gmail.com>
2018-09-18 14:26:47 +0300

    Bump version to 1.2.151

--- src/ddManager.js
+++ src/ddManager.js
@@ -211,7 +211,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.150',
+  VERSION: '1.2.151',
 
   setAvailableIntegrations: (availableIntegrations) => {
     IntegrationsLoader.setAvailableIntegrations(availableIntegrations);
