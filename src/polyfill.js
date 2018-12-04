5c515e1e103f098bcc78a14baf12ba96ec6d2279
Constantine Yurevich <yurevichcv@gmail.com>
2018-12-04 16:35:28 +0300

    removed unnecessary polyfills

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,16 +1,16 @@
-import 'core-js/fn/object/create';
-import 'core-js/fn/array/is-array';
-import 'core-js/fn/array/index-of';
-import 'core-js/fn/array/filter';
+// import 'core-js/fn/object/create'; // IE8
+// import 'core-js/fn/array/is-array'; // IE8
+// import 'core-js/fn/array/index-of'; // IE8
+// import 'core-js/fn/array/filter'; // IE8
 import 'core-js/fn/array/find';
 import 'core-js/fn/array/fill';
-import 'core-js/fn/array/map';
-import 'core-js/fn/array/some';
-import 'core-js/fn/function/bind';
+// import 'core-js/fn/array/map'; // IE8
+// import 'core-js/fn/array/some'; // IE8
+// import 'core-js/fn/function/bind'; // IE8
 import 'core-js/fn/object/assign';
-import 'core-js/fn/string/trim';
+// import 'core-js/fn/string/trim'; // IE8
 import 'core-js/fn/string/ends-with';
-import 'core-js/fn/date/to-iso-string';
-import 'core-js/fn/date/now';
+// import 'core-js/fn/date/to-iso-string'; // IE8
+// import 'core-js/fn/date/now'; // IE8
 import 'promise-polyfill/dist/polyfill';
 import 'whatwg-fetch';
