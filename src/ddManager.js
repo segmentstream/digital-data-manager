fce5e94aab7aa7473304057374766a9f82e770fc
Constantine Yurevich <yurevich@Constantines-MacBook-Pro.local>
2019-02-08 15:52:13 +0300

    version 1.2.195 bump

--- src/ddManager.js
+++ src/ddManager.js
@@ -218,7 +218,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.194',
+  VERSION: '1.2.195',
 
   setConsent: ConsentManager.setConsent,
   getConsent: ConsentManager.getConsent,
