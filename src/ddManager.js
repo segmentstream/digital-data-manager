b1681135c6fe97e050cab5b26afa635499e81ad9
Evgeniy Kuznetsov <evgeny.kuznetsov@segmentstream.com>
2019-05-30 09:42:45 +0300

    Bump version to 1.2.209

--- src/ddManager.js
+++ src/ddManager.js
@@ -218,7 +218,7 @@ function _initializeCustomEnrichments(settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.208',
+  VERSION: '1.2.209',
 
   setConsent: ConsentManager.setConsent,
   getConsent: ConsentManager.getConsent,
