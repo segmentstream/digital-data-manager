4306d5491552fee5baf7443532b6d947d9b490c8
Kirill Temnov <allselead@gmail.com>
2019-07-18 23:13:38 +0300

    Bump version to 1.2.225

--- src/ddManager.js
+++ src/ddManager.js
@@ -218,7 +218,7 @@ function _initializeCustomEnrichments (settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.224',
+  VERSION: '1.2.225',
 
   setConsent: ConsentManager.setConsent,
   getConsent: ConsentManager.getConsent,
