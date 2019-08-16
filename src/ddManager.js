35f7f6a444bb85af5810d1b5723e8b356f21d470
Artem Bogomolov <artem.bogomolov@segmentstream.com>
2019-08-16 17:17:16 +0300

    Bump version to 1.2.238

--- src/ddManager.js
+++ src/ddManager.js
@@ -218,7 +218,7 @@ function _initializeCustomEnrichments (settings) {
 
 const ddManager = {
 
-  VERSION: '1.2.237',
+  VERSION: '1.2.238',
 
   setConsent: ConsentManager.setConsent,
   getConsent: ConsentManager.getConsent,
