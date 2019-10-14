a4866a0542161154fb2b16c5e692ea5ed791a0f2
Kirill Temnov <allselead@gmail.com>
2019-10-14 17:38:46 +0300

    Fix scroll

--- src/trackers/trackScroll.js
+++ src/trackers/trackScroll.js
@@ -8,15 +8,15 @@ const getScrollPercent = () => {
   return parseInt((html.scrollTop || body.scrollTop) / ((html.scrollHeight || body.scrollHeight) - html.clientHeight) * 100, 10)
 }
 
-const addEvent = (scrollDepth, handler) => {
-  events.push({ scrollDepth, handler, isFired: false })
+const addEvent = (targetScrollDepth, handler) => {
+  events.push({ targetScrollDepth, handler, isFired: false })
 }
 
 const processEvents = () => {
   const scrolledPercent = getScrollPercent()
   events = events.map((event) => {
-    if (!event.isFired && event.scrollDepth <= scrolledPercent) {
-      event.handler(scrolledPercent)
+    if (!event.isFired && event.targetScrollDepth <= scrolledPercent) {
+      event.handler(event.targetScrollDepth)
       return { ...event, isFired: true }
     }
     return event
